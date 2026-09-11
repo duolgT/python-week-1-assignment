360562c0439408726fec9e660aef6971db98895a
DuolDaniel <duolkuach100@gmail.com>
2026-09-11 14:37:55 +0300

    created a message to myself

---
+++ All_about_me.py
@@ -0,0 +1,12 @@
+# This is a simple Python program that introduces myself and shares some personal information.
+
+name = "Duol Daniel"
+age = 26
+favourite_language = "JavaScript"
+goal = "Become a skilled software developer"
+
+
+print("Hello, my name is", name)
+print("I am", age, "years old")
+print("My favourite programming language is", favourite_language)
+print("My goal is to", goal)
